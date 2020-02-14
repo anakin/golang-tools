bc81c77550d5e523bbdb3a6b06ea729f696f211b
Anakin Sun <anakin.sun@chope.co>
2020-02-14 12:54:46 +0800

    init code

---
+++ main.go
@@ -0,0 +1,23 @@
+package golang_tools
+
+
+//like array_diff in PHP
+func difference(slice1 []string, slice2 []string) []string {
+	diffStr := []string{}
+	m := map[string]int{}
+
+	for _, s1Val := range slice1 {
+		m[s1Val] = 1
+	}
+	for _, s2Val := range slice2 {
+		m[s2Val] = m[s2Val] + 1
+	}
+
+	for mKey, mVal := range m {
+		if mVal == 1 {
+			diffStr = append(diffStr, mKey)
+		}
+	}
+
+	return diffStr
+}
